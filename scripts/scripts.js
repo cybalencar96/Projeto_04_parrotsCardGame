9320d462c69c09ad0ab9845a907f3d459bbf0911
Carlos <iuryalencar@poli.ufrj.br>
2021-07-27 08:46:10 -0300

    added choosing qtd Cards and appending childs

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,6 +2,16 @@ let qtdCards;
 do {
     qtdCards = Number(prompt("Digite a quantidade de cartas para jogar (apenas numeros inteiro e pares entre 4 e 14)"))
 } while (((qtdCards % 2) !== 0 || qtdCards < 4 || qtdCards > 14) && qtdCards !== -1)
+console.log(qtdCards);
+
+for (let i = 1; i < qtdCards; i++) {
+    const card = document.querySelector('.card');
+    const cards = document.querySelector('.cards');
+    // Need to create a new card div with all of its childs (arg true)
+    const newCard = card.cloneNode(true);
+    // append new card div
+    cards.appendChild(newCard);
+}
 
 function selectCard(element) {
     const frontFace = element.querySelector('.front-face');
@@ -10,6 +20,4 @@ function selectCard(element) {
 
     frontFace.style.transform = 'rotateY(-180deg)';
     backFace.style.transform = 'rotateY(0deg)';
-
-
 }
