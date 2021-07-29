1b54736ccbf7ff0f7a9cf4823ba969772a3d4175
Carlos <iuryalencar@poli.ufrj.br>
2021-07-29 20:58:28 -0300

    testing commit using linux

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,4 @@
+
 // card variables
 let backFace1;
 let backFace2;
@@ -7,6 +8,7 @@ let gif1;
 let gif2;
 
 // counters and controllers
+let qtdTotalCards = 0;
 let counterPlays = 0;
 let counterRights = 0;
 let qtdSelectedCards = 0;
@@ -19,9 +21,50 @@ let centisec=0;
 let strSec="00";
 let strMin="00";
 let strCentisec="00"
+let intervalID;
+
+
+// --- gif array with duplicated gifs
+let gifArr = ["bobrossparrot.gif","bobrossparrot.gif","explodyparrot.gif","explodyparrot.gif","fiestaparrot.gif","fiestaparrot.gif","metalparrot.gif","metalparrot.gif","revertitparrot.gif","revertitparrot.gif","tripletsparrot.gif","tripletsparrot.gif","unicornparrot.gif","unicornparrot.gif"];
+starting();
 
 const timer = document.querySelector('.timer');
-setInterval(cronometer,10);
+
+
+function starting(){
+    do {
+        qtdTotalCards = Number(prompt("Digite a quantidade de cartas para jogar (apenas numeros inteiro e pares entre 4 e 14)"))
+    } while (((qtdTotalCards % 2) !== 0 || qtdTotalCards < 4 || qtdTotalCards > 14) && qtdTotalCards !== -1);
+
+    setGame();
+}
+
+function setGame(){
+    // cut array to size the game and shuffle it
+    partialGifArr = gifArr.slice(0,qtdTotalCards);
+    partialGifArr = partialGifArr.sort(comparador);
+    idInterval = setInterval(cronometer,10);
+
+    for (let i = 0; i < qtdTotalCards; i++) {
+        const cards = document.querySelector('.cards');
+        const card = document.querySelector('.card');
+    
+        // Need to create a new card div witch is a copy of the existing one (with all of its childs (arg true))
+        const newCard = card.cloneNode(true);
+
+        // set a random gif to newCard using src attribute
+        newCard.querySelector('.back-face img').src = `/assets/${partialGifArr[i]}`;
+        newCard.classList.add('new-card');
+
+        // append new card div
+        cards.appendChild(newCard);
+        
+        // hide model card
+        if (i === qtdTotalCards - 1) {
+            card.style.display = "none";
+        }
+    }    
+}
 
 function selectCard(element) {
     gameStarted = true;
@@ -58,7 +101,7 @@ function selectCard(element) {
             if (counterRights === (qtdTotalCards/2)) {
                 // stop timer
                 const timeEnd = timer.innerHTML;
-                gameStarted = false;
+                clearInterval(intervalID);
 
                 setTimeout(function(){
                     // send victory message
@@ -85,6 +128,34 @@ function selectCard(element) {
     }
 }
 
+
+function restart() {
+    const newCards = document.querySelectorAll(".new-card");
+    const cards = document.querySelector('.cards');
+    const card = document.querySelector('.card');
+
+    // remove all new Cards
+    for (let i = 0; i < newCards.length; i++){
+        cards.removeChild(newCards[i]);
+    }
+
+    card.style.display = "initial";
+
+    // reseting global variables
+    qtdTotalCards = 0;
+    qtdSelectedCards = 0;
+    counterPlays = 0;
+    counterRights = 0;
+    min=0;
+    sec=0;
+    centisec=0;
+    gameStarted = false;
+
+
+    // start new game
+    starting();
+}
+
 function  cronometer() {
     if (gameStarted){
         if (centisec < 100) {centisec += 1;}
@@ -105,4 +176,8 @@ function  cronometer() {
         // if game ended
         timer.innerHTML = `00:00:00`
     }
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
